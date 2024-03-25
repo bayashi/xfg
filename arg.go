4f36f2555e80d99b060e5affcf6dd018ba447f7a
bayashi <42190+bayashi@users.noreply.github.com>
2024-03-25 22:57:58 +0000

    improve help messages

--- arg.go
+++ arg.go
@@ -60,20 +60,20 @@ func (cli *runner) parseArgs() *options {
 	flag.Uint32VarP(&o.contextLines, "context", "C", 0, "Show several lines before and after the matched one")
 	flag.Uint32VarP(&o.maxMatchCount, "max-count", "m", 0, "Stop reading a file after NUM matching lines")
 
-	flag.StringArrayVarP(&o.ignore, "ignore", "", []string{}, "Ignore stuff even with '--search-all'")
-	flag.BoolVarP(&o.hidden, "hidden", "", false, "Search hidden files")
-	flag.BoolVarP(&o.skipGitIgnore, "skip-git-ignore", "", false, "Search files and directories if these match a line of .gitignore")
-	flag.BoolVarP(&o.searchAll, "search-all", "", false, "Search all files and directories except specific ignoring files/directories")
+	flag.StringArrayVarP(&o.ignore, "ignore", "", []string{}, "Ignore path to pick up even with '--search-all'")
+	flag.BoolVarP(&o.hidden, "hidden", "", false, "Enable to search hidden files")
+	flag.BoolVarP(&o.skipGitIgnore, "skip-git-ignore", "", false, "Search files and directories even if a path matches a line of .gitignore")
+	flag.BoolVarP(&o.searchAll, "search-all", "", false, "Search all files and directories except specific ignoring files and directories")
 
-	flag.BoolVarP(&o.noColor, "no-color", "", false, "disable colors for matched words")
+	flag.BoolVarP(&o.noColor, "no-color", "", false, "Disable colors for an output")
 	flag.BoolVarP(&o.relax, "relax", "", false, "Insert blank space between contents for relaxing view")
 	flag.BoolVarP(&o.abs, "abs", "", false, "Show absolute paths")
 	flag.BoolVarP(&o.showMatchCount, "count", "c", false, "Show a count of matching lines instead of contents")
-	flag.BoolVarP(&o.onlyMatch, "only-match", "o", false, "Show paths only matched contents by grep")
-	flag.BoolVarP(&o.noGroupSeparator, "no-group-separator", "", false, "When '-C' is in use, do not print a separator between groups of lines")
+	flag.BoolVarP(&o.onlyMatch, "only-match", "o", false, "Show paths only matched contents")
+	flag.BoolVarP(&o.noGroupSeparator, "no-group-separator", "", false, "Do not print a separator between groups of lines")
 	flag.BoolVarP(&o.noIndent, "no-indent", "", false, "Do not print an indent string")
 
-	flag.StringVarP(&o.groupSeparator, "group-separator", "", defaultGroupSeparator, "When '-C' is in use, print this string instead of '--' between groups of lines")
+	flag.StringVarP(&o.groupSeparator, "group-separator", "", defaultGroupSeparator, "Print this string instead of '--' between groups of lines")
 	flag.StringVarP(&o.indent, "indent", "", defaultIndent, "Indent string for the top of each line")
 
 	flag.BoolVarP(&flagHelp, "help", "h", false, "Show help (This message) and exit")
