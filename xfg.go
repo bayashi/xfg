c70afccda3b5bc72d4fb9953542e7286fc126edd
bayashi <42190+bayashi@users.noreply.github.com>
2024-05-04 05:55:18 +0000

    refactor xfg

---
+++ xfg.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"io/fs"
+	"regexp"
+	"sync"
+
+	"github.com/fatih/color"
+	ignore "github.com/sabhiram/go-gitignore"
+)
+
+type line struct {
+	lc      int32 // line number
+	content string
+	matched bool
+}
+
+type path struct {
+	path     string
+	info     fs.DirEntry
+	contents []line
+}
+
+type result struct {
+	mu                  sync.RWMutex
+	paths               []path
+	outputLC            int // Used on pager. Rough count. Not included group separators.
+	alreadyMatchContent bool
+}
+
+type xfg struct {
+	cli     *runner
+	options *options
+
+	pathBaseColor      string
+	pathHighlightColor *color.Color
+	pathHighlighter    []string
+	grepHighlightColor *color.Color
+	grepHighlighter    []string
+
+	searchPathi  []*regexp.Regexp
+	searchGrepi  []*regexp.Regexp
+	searchPathRe []*regexp.Regexp
+	searchGrepRe []*regexp.Regexp
+	ignoreRe     []*regexp.Regexp
+	gitignore    *ignore.GitIgnore
+	xfgignore    *ignore.GitIgnore
+
+	result result
+}
+
+func newX(cli *runner, o *options) *xfg {
+	o.prepareFromENV()
+	o.prepareAliases()
+	o.prepareContextLines(cli.isTTY)
+
+	x := &xfg{
+		cli:     cli,
+		options: o,
+	}
+
+	return x
+}
